019a570e08a9f295d127a761bb6d754f44eb903d
user <user@DESKTOP-VUVK06B>
2024-11-22 18:44:33 +0300

    Proje dosyası ekle.

---
+++ Proje_Ödevi/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Proje_Ödevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region PROJE ÖDEVİ
+
+            //string[] ogrenciler = { "Safiye", "Eymen", "Safiye", "Bekir", "Rümeysa", "Safiye", "Berat", "Safiye" };
+
+            //Yukarıdaki dizinin içinde bulunan SAFİYE isimlerinin Index değerlerini ekrana bir döngü yardımı ile yazdırınız.
+
+            //Düşününki dizi içinde 1Milyon isim olabilir. Oluşturulan döngü tek tek kontrol ederek 1 Milyon tur atmasın. Kodun başarılı olması en az tur da bütün indexleri yazdırmalısınız.
+
+            string[] ogrenciler = { "Safiye", "Eymen", "Safiye", "Bekir", "Rümeysa", "Safiye", "Berat", "Safiye" };
+
+            string isim = "Safiye";
+
+            int i = Array.IndexOf(ogrenciler, isim);
+
+            while (i == Array.IndexOf(ogrenciler, isim, i))
+            {
+                if (i == Array.LastIndexOf(ogrenciler, isim))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                Console.WriteLine(i);
+                i = Array.IndexOf(ogrenciler, isim, i + 1);
+            }
+
+
+            #endregion
+        }
+    }
+}
